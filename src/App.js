6ecd7bdfdf8b3669e12f3f9c69832b2fb7413d71
dhdld <btj5420@naver.com>
2023-12-01 23:47:07 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,47 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        <p>        
+          Homepage
+          <br />
+          Front-end 개발자 김소연의 포트폴리오입니다.
+
+        </p>
+        </div>
+
+        <p>기술 스택
+        <br />
+        JavaScript, React</p>
+      <div>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>
+          Contact me
+          </b>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/dhdld"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          github
         </a>
+        <br />
+        <a
+          className='App-link'
+          href='https://velog.io/@dhdld'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          velog
+        </a>
+        </div>
+
       </header>
+      <div>
+      </div>
     </div>
   );
 }
